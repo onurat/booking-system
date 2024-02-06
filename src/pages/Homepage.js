1bdf227e850adec2a985e575f3dfd0807c163970
onurat <114289826+onurat@users.noreply.github.com>
2024-02-06 22:24:22 +0000

    new legend

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import Calendar from '../Components/Calendar/Calendar';
-
+import './Homepage.css';
 
 function Homepage() {
   return (
-    <div>
-    <Calendar />
+    <div className="homepage-container">
+      <div className="legend-container">
+        <div className="legend">
+          <h3>Legend</h3>
+          <div className="legend-item">
+            <div className="legend-color green"></div>
+            <div>Available</div>
+          </div>
+          <div className="legend-item">
+            <div className="legend-color orange"></div>
+            <div>Almost Full</div>
+          </div>
+          <div className="legend-item">
+            <div className="legend-color red"></div>
+            <div>Booked Out</div>
+          </div>
+        </div>
+      </div>
+      <div className="calendar-container">
+        <Calendar />
+      </div>
     </div>
   );
 }
