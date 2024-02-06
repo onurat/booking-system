ad24c96b59800536848546d68803e8df12598f17
onurat <114289826+onurat@users.noreply.github.com>
2024-02-06 18:38:03 +0000

    .

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,11 +1,12 @@
-// Footer.jsx
 import React from 'react';
 import './Footer.css';
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="footer">
-      <p>&copy; Booking System</p>
+      <p>&copy; {currentYear} Booking System</p>
       <p>
         <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a>
       </p>
